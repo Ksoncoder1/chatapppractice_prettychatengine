ee7081577ffe91012f3a0f3269da44a709803165
Erickson Kimathi <ericksonkimathi@Ericksons-MacBook-Pro.local>
2023-07-04 14:31:01 +0300

    first commmit

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,13 +1,13 @@
-import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced';
+import {PrettyChatWindow} from 'react-chat-engine-pretty';
 const ChatsPage = (props) =>{
-    const chatProps = useMultiChatLogic('5ec8c8d4-4e50-40b5-87a8-b47b0ce95253', 
-    props.user.username, 
-    props.user.secret
-    );
     return(
         <div style={{height: '100vh'}}>
-            <MultiChatSocket {...chatProps} />
-            <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+            <PrettyChatWindow
+              projectId="5ec8c8d4-4e50-40b5-87a8-b47b0ce95253"
+              username={props.user.username}
+              secret={props.user.secret}
+              style={{height: '100%'}}
+            />
         </div>
     )
 }
